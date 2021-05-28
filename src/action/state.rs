66a7b83b1557bee4de7de5b8ba63cddd63000b75
Dustin Yost <dustin.yost.t@gmail.com>
2021-05-28 08:53:01 -0400

    Move from elapsed time to frame tracking to determine if a button was pressed this frame

--- src/action/state.rs
+++ src/action/state.rs
@@ -1,5 +1,5 @@
 use crate::{action, event, source};
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 /// The state of an active [`action`](action::Action) for a given user.
 #[derive(Debug, Clone)]
@@ -7,7 +7,9 @@ pub struct State {
 	kind: source::Kind,
 	behavior: action::Behavior,
 	/// Used to indicate if a button is pressed or released
+	prev_frame_active: bool,
 	active: bool,
+	active_state_changed_this_frame: bool,
 	value: f32,
 	modified_at: Instant,
 }
@@ -17,7 +19,9 @@ impl State {
 		Self {
 			kind: action.kind(),
 			behavior: action.behavior().clone(),
+			prev_frame_active: false,
 			active: false,
+			active_state_changed_this_frame: false,
 			value: 0.0,
 			modified_at: Instant::now(),
 		}
@@ -26,8 +30,13 @@ impl State {
 	pub(crate) fn process_event(&mut self, event: event::Event, time: &Instant) {
 		if match event.state {
 			event::State::ButtonState(btn_state) => {
-				self.active = btn_state == event::ButtonState::Pressed;
-				true
+				let is_active = btn_state == event::ButtonState::Pressed;
+				if self.active != is_active {
+					self.active = is_active;
+					true
+				} else {
+					false
+				}
 			}
 			event::State::MouseMove(_x, _y) => false,
 			event::State::MouseScroll(_x, _y) => false,
@@ -46,26 +55,25 @@ impl State {
 	}
 
 	pub(crate) fn requires_updates(&self) -> bool {
-		self.behavior.digital_axis().is_some()
+		true //self.behavior.digital_axis().is_some()
 	}
 
-	pub(crate) fn update(&mut self, _time: &Instant) {}
-
-	/// Returns true if the amount of time elapsed since the action was last modified
-	/// is less than or equal to the provided duration.
-	fn modified_within(&self, duration: Duration) -> bool {
-		self.modified_at.elapsed() <= duration
+	pub(crate) fn update(&mut self, _time: &Instant) {
+		self.active_state_changed_this_frame = self.active != self.prev_frame_active;
+		if self.active_state_changed_this_frame {
+			self.prev_frame_active = self.active;
+		}
 	}
 
 	/// Returns true when a [`button binding`](crate::source::Kind::Button) is pressed,
-	/// and this function is called <= 1ms after it being pressed.
+	/// and this function is called in the same update frame as the input event which pressed the button.
 	pub fn on_button_pressed(&self) -> bool {
-		self.active && self.modified_within(Duration::from_millis(1))
+		self.active && self.active_state_changed_this_frame
 	}
 
 	/// Returns true when a [`button binding`](crate::source::Kind::Button) is not pressed,
-	/// and this function is called <= 1ms after it being released.
+	/// and this function is called in the same update frame as the input event which released the button.
 	pub fn on_button_released(&self) -> bool {
-		!self.active && self.modified_within(Duration::from_millis(1))
+		!self.active && self.active_state_changed_this_frame
 	}
 }
